446339e892e7b4bbd62dd2b1947a88f38897bb28
Odilon Jonathan Kröger <kroger.odilonj@gmail.com>
2016-11-17 21:01:53 -0200

    Adicionada classe Conta e testes de JPA.

---
+++ financas/src/br/com/caelum/financas/teste/TesteGerenciamentoJPA.java
@@ -0,0 +1,27 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteGerenciamentoJPA {
+
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+		em.getTransaction().begin();
+		Conta conta = em.find(Conta.class, 3);
+		
+		//	Commit antes da alteração.
+		em.getTransaction().commit();
+		conta.setBanco("British International Bank");
+		em.getTransaction().begin();
+		em.merge(conta);
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+	
+}
